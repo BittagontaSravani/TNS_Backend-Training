7bf7af04582fb81b2b9c76cf211518e3c77d4036
Sravani <Sravani@192.168.1.4>
2025-11-01 22:52:09 +0530

    Added Student class to display student details

---
+++ src/mypack/Student.java
@@ -0,0 +1,17 @@
+package mypack;
+//Demo for types of variables
+public class Student {
+	//instance variable
+	int rollno;
+	String name;
+	String branch;
+	//static variable
+	static String collegeName="AVN";
+	//method
+	void print() {
+		//local variable
+		String msg="These are TNSIF Students";
+		System.out.println(msg);
+		System.out.println("The Student details are" + "\nName: " + name + "\nRoll Number: " + rollno + "\nBranch: " + branch + "\nCollege: " + collegeName);
+	}
+}
---
+++ src/mypack/StudentMain.java
@@ -0,0 +1,14 @@
+package mypack;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		//creating object for Student class
+		Student S = new Student();
+		S.rollno = 6614;
+		S.name = "Sravani";
+		S.branch = "AIML";
+		S.print();
+	}
+
+}
