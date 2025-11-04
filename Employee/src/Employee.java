e59a6177da2ac419f090c8ae18dfd52462bb3a6a
Sravani_Bittugunta <sravanibittagonta@gmail.com>
2025-11-04 21:51:36 +0530

    Salesforce Hyderabad, Employee details

---
+++ Employee/src/Employee.java
@@ -0,0 +1,19 @@
+// Demo for types of variables using Employee class
+
+public class Employee {
+    // instance variables
+    int empId;
+    String empName;
+    String department;
+    
+    // static variable
+    static String companyName = "Salesforce Hyderabad";
+    
+    // method
+    void display() {
+        // local variable
+        String msg = "These are Employee Details";
+        System.out.println(msg);
+        System.out.println(" Employee Information:" + "\nName: " + empName + "\nEmployee ID: " + empId + "\nDepartment: " + department + "\nCompany: " + companyName);
+    }
+}
---
+++ Employee/src/EmployeeMain.java
@@ -0,0 +1,14 @@
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		// creating object for Employee class
+        Employee e = new Employee();
+        e.empId = 111;
+        e.empName = "SravaniBittugunta";
+        e.department = "Software Engineering SMTS";
+        e.display();
+
+
+	}
+
+}
