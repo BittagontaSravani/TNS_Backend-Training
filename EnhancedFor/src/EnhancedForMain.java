edc67b4c76399d3dc0c9ab644b290e07976dac5e
Sravani_Bittugunta <sravanibittagonta@gmail.com>
2025-11-05 22:27:17 +0530

    Added Enhanced For Loop program with separate class and object

---
+++ EnhancedFor/src/EnhancedForMain.java
@@ -0,0 +1,10 @@
+public class EnhancedForMain {
+
+    public static void main(String[] args) {
+        // Creating object for EnhancedFor class
+        EnhancedFor obj = new EnhancedFor();
+
+        // Calling the display method
+        obj.displayNumbers();
+    }
+}
