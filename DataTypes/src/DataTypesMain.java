56576836b9c3cb22720d4f693f4843c19b67ce35
Sravani_Bittugunta <sravanibittagonta@gmail.com>
2025-11-05 19:50:16 +0530

    Different types of DataTypes

---
+++ DataTypes/src/DataTypesMain.java
@@ -0,0 +1,10 @@
+public class DataTypesMain {
+    public static void main(String[] args) {
+        // Creating an object for DataTypes class
+        DataTypes obj = new DataTypes();
+
+        // Calling the display method using the object
+        obj.displayValues();
+    }
+}
+    
