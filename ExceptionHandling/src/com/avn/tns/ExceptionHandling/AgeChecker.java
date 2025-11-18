01d7d454f2b5cb8fa112436e9623b36882073e63
Sravani_Bittugunta <sravanibittagonta@gmail.com>
2025-11-18 12:35:24 +0530

    Implemented AgeChecker with custom InvalidAge exception and integrated exception handling in AgeMain

---
+++ ExceptionHandling/src/com/avn/tns/ExceptionHandling/AgeChecker.java
@@ -0,0 +1,13 @@
+package com.avn.tns.ExceptionHandling;
+
+public class AgeChecker {
+
+    public void checkAge(int age) throws InvalidAge, ArithmeticException {
+        if (age < 18) {
+            throw new InvalidAge("The person is not eligible to vote");
+        } 
+        else {
+            System.out.println("The person is eligible to vote");
+        }
+    }
+}
